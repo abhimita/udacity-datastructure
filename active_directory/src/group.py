cd6cc2b9ad1707f9a5ca6611304c08d892062560
abhijitbhatt <bhattacharya.abhi94@gmail.com>
2019-05-26 18:25:50 -0700

    Renamed file

--- active_directory/src/group.py
+++ active_directory/src/group.py
@@ -1,62 +1,80 @@
+
+"""
+Class to implement entity: group
+Users or other groups can belong to a group
+"""
 class Group(object):
     def __init__(self, _name):
         self.name = _name
-        self.groups = []
-        self.users = []
+        self.groups = [] # list storing the groups that belong to this group
+        self.users = []  # list of users who are part of this group
 
+    # Method to add a group to a parent group
     def add_group(self, group):
         self.groups.append(group)
 
+    # Method to add user to a group
     def add_user(self, user):
         self.users.append(user)
 
+    # Method that returns list of groups
     def get_groups(self):
         return self.groups
 
+    # Method that returns list of users
     def get_users(self):
         return self.users
 
+    # Returns the name of the group
     def get_name(self):
         return self.name
 
+"""
+Class that forms the data structure so that membership can be checked efficiently
+The initialization 
+"""
+
 class MembershipFinder:
-    def __init__(self, groups):
-        self.groups = groups
-        self.stack = []
-        for g in self.groups:
-            self.stack.append((g, '/' + g.get_name()))
+    def __init__(self, groups, users):
+        # Dictionary that maps user or group to an integer number (id)
+        object_id_map = {}
+        # Dictionary that maps id (see above) to object (user or group)
+        id_object_map = {}
+        # Final dictionary to map user to the group or groups that it belongs to
+        # If user: u belongs to group g1 & group g2 then this dictionary will look like
+        # user_group_map[u,g1] = True and user_group_map[u,g2] = True
         self.user_group_map = {}
-        self.traverse()
+        data = [i for i in range(0, len(groups) + len(users))]
+        for g in groups:
+            index = len(object_id_map)
+            object_id_map[g] = index
+            id_object_map[index] = g
+        for u in users:
+            index = len(object_id_map)
+            object_id_map[u] = index
+            id_object_map[index] = u
+        # Implement union algorithm without path compression
+        for g in groups:
+            for s in g.get_groups():
+                index = object_id_map[s]
+                data[index] = object_id_map[g]
+            for u in g.get_users():
+                index = object_id_map[u]
+                data[index] = object_id_map[g]
+        for u in users:
+            index = object_id_map[u]
+            while index != data[index]:
+                self.user_group_map[u + ',' + id_object_map[data[index]].get_name()] = True
+                index = data[index]
 
-    def traverse(self):
-        while len(self.stack) > 0:
-            grp, path = self.stack.pop()
-            for u in grp.get_users():
-                for p in path.split('/')[1:]:
-                    self.user_group_map[u + ',' + p] = True
-            for g in grp.get_groups():
-                self.stack.append((g, path + '/' + g.get_name()))
-
+    """
+    Return True if user is in the group, False otherwise.
+    Args:
+        user(str): user name
+        group(class:Group): group to check user membership against
+    """
     def is_user_in_group(self, user, group):
-        """
-        Return True if user is in the group, False otherwise.
 
-        Args:
-          user(str): user name/id
-          group(class:Group): group to check user membership against
-        """
         return self.user_group_map.get(user + ',' + group.get_name(), None) is not None
 
-if __name__ == '__main__':
-
-
-    groups = [Group("Group-%s" % str(i + 1)) for i in range(0, 3)]
-    groups[0].add_group(groups[1])
-    groups[1].add_group(groups[2])
-    groups[0].add_user('user_1')
-    groups[2].add_user('user_2')
-
-    membership_finder = MembershipFinder(groups)
-    print(membership_finder.is_user_in_group('user_2', groups[0]))
-    print(membership_finder.is_user_in_group('user_1', groups[2]))
 
